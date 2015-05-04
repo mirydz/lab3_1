9f6b6c945bf2cb2cd1a19c559c2feccb3ef0f60c
Michal Rydz <mirydz@gmail.com>
2015-05-04 03:03:31 +0200

    add test to ensure that given a invoice request with one item, an invoice with one item is issued

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,52 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import java.util.Date;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class BookKeeperTest  {
+
+    @Test
+    public void ShouldIssueOnePositionInvoiceWhenRequestHasOnePosition() {
+        // arrange
+        ClientData clientData = new ClientData(Id.generate(), "John Smith");
+        Invoice invoice = new Invoice(Id.generate(), clientData);
+        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+        ProductData productData = new ProductData(
+                Id.generate(),
+                new Money(120, "PLN"),
+                "Sample Product",
+                ProductType.DRUG,
+                new Date()
+        );
+
+        Money price = new Money(10);
+        RequestItem requestItem = new RequestItem(productData, 0, price);
+        invoiceRequest.add(requestItem);
+
+        // create mocks
+        InvoiceFactory mockInvoiceFactory = Mockito.mock(InvoiceFactory.class);
+        Mockito.when(mockInvoiceFactory.create(invoiceRequest.getClientData()))
+                .thenReturn(invoice);
+
+        TaxPolicy mockTaxPolicy = Mockito.mock(TaxPolicy.class);
+        Mockito.when(mockTaxPolicy.calculateTax(ProductType.DRUG, price))
+                .thenReturn(new Tax(new Money(0), ""));
+
+        // act
+        BookKeeper bookKeeper = new BookKeeper(mockInvoiceFactory);
+        Invoice issuedInvoice = bookKeeper.issuance(invoiceRequest, mockTaxPolicy);
+
+        //assert
+        assertThat(issuedInvoice.getItems().size(), is(1));
+    }
+}
